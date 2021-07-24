6df774e09fb1eda79dec94e91b6388c845f39135
Beargirl <cool.beargirl@yandex.ru>
2021-07-24 23:07:29 +0300

    changed

---
+++ index.js
@@ -0,0 +1,24 @@
+function sum() {
+        const a = document.getElementById('first_number').value;
+        const b = document.getElementById('second_number').value;
+        let c = document.getElementById('result') => a + b;
+    }
+ы
+
+    function subtract() {
+        const a = document.getElementById('first_number').value;
+        const b =  document.getElementByClassName('second_number').value;
+        let c = document.getElementById('result') => a - b;
+    }
+
+    function multiplicate() {
+        const a = document.getElementById('first_number').value;
+        const b =  document.getElementById('second_number').value;
+        let c = document.getElementById('result') => a * b;
+
+    }
+    function divide() {
+        const a = document.getElementById('first_number').value;
+        const b =  document.getElementById('second_number').value;
+        let c = document.getElementById('result') => a / b;
+    }
